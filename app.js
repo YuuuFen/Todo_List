37da74c8200f3832bf5ffe306ae9369ad8ecef80
YuFen <yufenhuangtaiwan.com>
2022-03-23 16:11:09 +0800

    complete and delete animation

--- app.js
+++ app.js
@@ -30,12 +30,32 @@ add.addEventListener("click", e => {
     completeBtn.classList.add("complete")
     completeBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
 
+        // complete status
+    completeBtn.addEventListener("click", e => {
+        let todoItem = e.target.parentElement;
+        todoItem.classList.toggle("done")
+    })
+
     let trashBtn = document.createElement("button")
     trashBtn.classList.add("trash")
     trashBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
 
+        // delete item + animation
+    trashBtn.addEventListener("click", e => {
+        let todoItem = e.target.parentElement;
+            // 為了不讓 remove 先執行，要先等 animation 結束再執行
+        todoItem.addEventListener("animationend", () => {
+            todoItem.remove();
+        })
+
+        todoItem.style.animation = "scaleDown 0.3s forwards";
+    })
+
     todo.appendChild(completeBtn)
     todo.appendChild(trashBtn)
+
+        // show up animation
+    todo.style.animation = "scaleUP 0.3s forwards"
 
     section.appendChild(todo)
 })
